96e8fecdfc3fb264a4b8a1455a8e6a74c998bec9
sbx_user1051 <sbx_user1051@169.254.183.149>
2019-09-01 15:45:29 +0000

    Sun Sep 01 15:45:29 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,12 +1,22 @@
 package befaster.solutions.CHK;
 
 public class CheckoutSolution {
-    private Map<String, Integer> skus = new HashMap<String, Integer>();
+    private Map<String, Integer> skus = new HashMap<String, Integer>() {
+        {
+            put("A", 50);
+            put("B", 30);
+            put("C", 20);
+            put("D", 15);
+        }
+    };
+
     public Integer checkout(String skus) {
         if (skus == "") {
-            return -1;
+            return 0;
         }
+        
         return -1;
     }
 }
+
 
