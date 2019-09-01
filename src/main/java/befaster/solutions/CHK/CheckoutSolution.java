d6755aebac58b899ab947a7948b5cacd5e064570
sbx_user1051 <sbx_user1051@169.254.183.149>
2019-09-01 15:51:29 +0000

    Sun Sep 01 15:51:29 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -10,16 +10,33 @@ public class CheckoutSolution {
         }
     };
 
+    private Map<char, Map<Integer, Integer>> = skuOffers = new HashMap<char, Map<Integer, Integer>>() {
+        {
+            put('A', new HashMap<Integer, Integer>() {
+                {
+                    put(3, 130);
+                }
+            });
+            put('B', new HashMap<Integer, Integer>() {
+                {
+                    put(2, 45);
+                }
+            });
+        }
+    };
+
     public Integer checkout(String skus) {
         Integer total = 0;
         for (char sku: skus.toCharArray()) {
             if (!skuToValue.containsKey(sku)) {
                 return -1;
             }
+            total += skuToValue.get(sku);
         }
         return total;
     }
 }
+
 
 
 
