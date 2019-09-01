f24cd4f34775674299cf5e36034fe54136c0dc5e
sbx_user1051 <sbx_user1051@169.254.183.149>
2019-09-01 15:33:29 +0000

    Sun Sep 01 15:33:29 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,22 @@
 package befaster.solutions.CHK;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckoutSolutionTest {
+    private CheckoutSolution checkout;
+
+    @Before
+    public void setUp() {
+        checkout = new CheckoutSolution();
+    }
+
+    @Test
+    public void emptyTest() {
+        assertThat(checkout.checkout(""), equalTo(-1));
+    }
 }
+
