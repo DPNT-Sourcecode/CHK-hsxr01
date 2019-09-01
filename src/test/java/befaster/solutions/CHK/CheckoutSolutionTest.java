09dcbc17b12975a8f65f7e03b593c064116ed264
sbx_user1051 <sbx_user1051@169.254.83.153>
2019-09-01 16:31:51 +0000

    Sun Sep 01 16:31:51 UTC 2019

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -20,23 +20,68 @@ public class CheckoutSolutionTest {
     }
 
     @Test
+    public void invalidTest() {
+        assertThat(checkout.checkout("E"), equalTo(-1));
+    }
+
+    @Test
     public void singleItemATest() {
         assertThat(checkout.checkout("A"), equalTo(50));
     }
 
     @Test
     public void moreThanOneItemATest() {
-        assertThat(checkout.checkout("AA"), equalTo(100));
+        assertThat(checkout.checkout("AA"), equalTo(50 + 50));
     }
 
     @Test
     public void ItemAOfferTest() {
-        assertThat(checkout.checkout("AAA"), equalTo(130));
+        assertThat(checkout.checkout("AAA"), equalTo(50 + 50 + 50 - 150 + 130));
     }
 
     @Test
     public void moreThanItemOfferATest() {
-        assertThat(checkout.checkout("AAAA"), equalTo(180));
+        assertThat(checkout.checkout("AAAA"), equalTo(50 + 50 + 50 + 50 - 150 + 130));
+    }
+
+    @Test
+    public void singleItemBTest() {
+        assertThat(checkout.checkout("B"), equalTo(30));
     }
 }
 
+    @Test
+    public void ItemBOfferTest() {
+        assertThat(checkout.checkout("BB"), equalTo(30 + 30 - 60 + 45));
+    }
+
+    @Test
+    public void moreThanItemOfferBTest() {
+        assertThat(checkout.checkout("BBB"), equalTo(30 + 30 + 30 - 60 + 45));
+    }
+
+    @Test
+    public void moreThanItemOfferBAppliedOnceTest() {
+        assertThat(checkout.checkout("BBBB"), equalTo(30 + 30 + 30 + 30 - 60 + 45));
+    }
+
+    @Test
+    public void singleItemCTest() {
+        assertThat(checkout.checkout("C"), equalTo(20));
+    }
+
+    @Test
+    public void singleItemDTest() {
+        assertThat(checkout.checkout("D"), equalTo(15));
+    }
+
+    @Test
+    public void differentWithoutOffersTest() {
+        assertThat(checkout.checkout("ABCDA"), equalTo(50 + 30 + 20 + 15 + 50));
+    }
+
+    @Test
+    public void differentWithOffersTest() {
+        assertThat(checkout.checkout("ABCDABA"), equalTo(50 + 30 + 20 + 15 + 50 + 30 + 50 - 150 + 130 - 60 + 45));
+    }
+}
